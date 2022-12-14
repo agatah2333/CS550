2baf1e9b337c5932ab99b1fb5fee6ab6d58f34bd
fcheema2 <119087244+fcheema2@users.noreply.github.com>
2022-12-14 12:43:22 -0500

    Delete Project_HA2/cs_450_550_ha2_univ_db_template directory

--- Project_HA2/cs_450_550_ha2_univ_db_template/solution_algebra/class_example_main_algebra.py
+++
@@ -1,9 +0,0 @@
-import json
-import class_example_algebra as ha
-
-f = open("../testDBs/sampleUnivDB.json", "r")
-db = json.loads(f.read())
-
-output = ha.ha2(db)
-
-print(json.dumps(output))
--- Project_HA2/cs_450_550_ha2_univ_db_template/solution_sql/credentials.py
+++
@@ -1,6 +0,0 @@
-username = 'fcheema4'
-password = 'pteewool'
-
-# An example of username and password:
-# username = 'sshres2'
-# password = 'dbpass'
