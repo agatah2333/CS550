5ea5a1821b283ca38cf813c9aa7f7dbb459688c1
fcheema2 <119087244+fcheema2@users.noreply.github.com>
2022-12-14 13:03:23 -0500

    Update myscript.py

--- myscript.py
+++ myscript.py
@@ -1,10 +1,13 @@
+# myscript.py
+#source: https://cx-oracle.readthedocs.io/en/latest/user_guide/installation.html
+#source: https://labs.vse.gmu.edu/index.php/Services/Oracle
 
 
 import cx_Oracle
-cx_Oracle.init_oracle_client(lib_dir=r"C:\Users\fchee\Downloads\instantclient_21_6")
+cx_Oracle.init_oracle_client(lib_dir=r"C:\Downloads\instantclient_21_6")
 
 # Connect as user "hr" with password "welcome" to the "orclpdb1" service running on this computer.
-connection = cx_Oracle.connect(user="fcheema4", password="pteewool",
+connection = cx_Oracle.connect(user="username", password="password",
                                dsn="@artemis.vsnet.gmu.edu:1521/vse18c.vsnet.gmu.edu")
 
 cursor = connection.cursor()
