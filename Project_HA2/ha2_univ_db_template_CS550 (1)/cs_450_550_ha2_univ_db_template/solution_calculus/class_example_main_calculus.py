c2f5995354c5961803096cfc836fff2296370b77
fcheema2 <119087244+fcheema2@users.noreply.github.com>
2022-12-14 12:46:16 -0500

    Delete Project_HA2/ha2_univ_db_template_CS550 (1) directory

--- Project_HA2/ha2_univ_db_template_CS550 (1)/cs_450_550_ha2_univ_db_template/solution_calculus/class_example_main_calculus.py
+++
@@ -1,9 +0,0 @@
-import json
-import class_example_calculus as ha
-
-f = open("../testDBs/sampleUnivDB.json", "r")
-input = json.loads(f.read())
-
-output = ha.ha2(input)
-
-print(json.dumps(output))
--- Project_HA2/ha2_univ_db_template_CS550 (1)/cs_450_550_ha2_univ_db_template/solution_sql/credentials_template.py
+++
@@ -1,6 +0,0 @@
-username = ''
-password = ''
-
-# An example of username and password:
-# username = 'sshres2'
-# password = 'dbpass'
